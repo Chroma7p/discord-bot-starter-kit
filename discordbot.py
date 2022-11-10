dd2b1d584980f63b8c3c2b8058883abe2d9fcd85
Chroma7p <nahco3.hcl@gmail.com>
2022-11-10 20:12:19 +0900

    フォーマッタの適用 トークン管理方法の変更

--- discordbot.py
+++ discordbot.py
@@ -1,41 +1,45 @@
-#discord.pyの大事な部分をimport
+# discord.pyの大事な部分をimport
 from discord.ext import commands
-from apitoken import APITOKEN
+import os
 
-#botのオブジェクトを作成(コマンドのトリガーを!に)
-bot=commands.Bot(command_prefix='!')
+# デプロイ先の環境変数にトークンをおいてね
+APITOKEN = os.environ["DISCORD_API_TOKEN"]
+# botのオブジェクトを作成(コマンドのトリガーを!に)
+bot = commands.Bot(command_prefix="!")
 
-#コマンドを設定
+# コマンドを設定
 @bot.command()
-#"!hello"と送信された時
+# "!hello"と送信された時
 async def hello(ctx):
-    await ctx.send("hello!")#送信された場所に"hello!"と送り返す
+    await ctx.send("hello!")  # 送信された場所に"hello!"と送り返す
 
-#イベントを検知
+
+# イベントを検知
 @bot.event
-#botの起動が完了したとき
+# botの起動が完了したとき
 async def on_ready():
-    print("Hello!")#コマンドラインにHello!と出力
+    print("Hello!")  # コマンドラインにHello!と出力
+
 
 @bot.event
-async def on_message_edit(before,after):
-    txt=f"{before.author} editted message!\nbefore:{before.content}\nafter:{after.content}"
+async def on_message_edit(before, after):
+    txt = f"{before.author} editted message!\nbefore:{before.content}\nafter:{after.content}"
     await before.channel.send(txt)
 
 
 @bot.event
 async def on_message_delete(message):
-    txt=f"{message.author} deleted message!\n{message.content}"
+    txt = f"{message.author} deleted message!\n{message.content}"
     await message.channel.send(txt)
 
 
-#コグのフォルダ
-cogfolder="cogs."
-#そして使用するコグの列挙(拡張子無しのファイル名)
-cogs=["sample_cog"]
+# コグのフォルダ
+cogfolder = "cogs."
+# そして使用するコグの列挙(拡張子無しのファイル名)
+cogs = ["sample_cog"]
 
 for c in cogs:
-  bot.load_extension(cogfolder+c)
+    bot.load_extension(cogfolder + c)
 
-#起動
+# 起動
 bot.run(APITOKEN)
